46a9ff837a4220d01a952dcdac925c760110bd01
nzh233 <1083225709@qq.com>
2020-11-01 13:30:27 +0800

    day9 exercises

---
+++ public/day9/index.js
@@ -0,0 +1,248 @@
+// index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// class based component
+class Header extends React.Component {
+  render() {
+    console.log(this.props.data)
+    const {
+      welcome,
+      title,
+      subtitle,
+      author: { firstName, lastName },
+      date,
+    } = this.props.data
+
+    return (
+      <header style={this.props.styles}>
+        <div className='header-wrapper'>
+          <h1>{welcome}</h1>
+          <h2>{title}</h2>
+          <h3>{subtitle}</h3>
+          <p>
+            {firstName} {lastName}
+          </p>
+          <small>{date}</small>
+        </div>
+      </header>
+    )
+  }
+}
+
+const Message = ({ message }) => (
+  <div>
+    <h1>{message}</h1>
+  </div>
+)
+const Login = () => (
+  <div>
+    <h3>Please Login</h3>
+  </div>
+)
+const Welcome = (props) => (
+  <div>
+    <h1>Welcome to 30 Days Of React</h1>
+  </div>
+)
+
+// A button component
+const Button = ({ text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
+)
+
+// TechList Component
+// class base component
+class TechList extends React.Component {
+  render() {
+    const { techs } = this.props
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+    return techsFormatted
+  }
+}
+
+// Main Component
+// Class Component
+class Main extends React.Component {
+  render() {
+    const {
+      techs,
+      greetPeople,
+      handleTime,
+      loggedIn,
+      handleLogin,
+      message,
+      background,
+    } = this.props
+    console.log(message)
+
+    const status = loggedIn ? <Welcome /> : <Login />
+    return (
+      <main style={background}>
+        <div className='main-wrapper'>
+          <p>Prerequisite to get started react.js:</p>
+          <ul>
+            <TechList techs={this.props.techs} />
+          </ul>
+          {techs.length === 3 && (
+            <p>You have all the prerequisite courses to get started React</p>
+          )}
+          <div>
+            <Button
+              text='Show Time'
+              onClick={handleTime}
+              style={buttonStyles}
+            />{' '}
+            <Button
+              text='Greet People'
+              onClick={greetPeople}
+              style={buttonStyles}
+            />
+            {!loggedIn && <p>Please login to access more information about 30 Days Of React challenge</p>}
+          </div>
+          <div style={{ margin: 30 }}>
+            <Button
+              text={loggedIn ? 'Logout' : 'Login'}
+              style={buttonStyles}
+              onClick={handleLogin}
+            />
+            <br />
+            {status}
+          </div>
+          <Message message={message} />
+        </div>
+      </main>
+    )
+  }
+}
+
+// CSS styles in JavaScript Object
+const buttonStyles = {
+  backgroundColor: '#61dbfb',
+  padding: 10,
+  border: 'none',
+  borderRadius: 5,
+  margin: '3px auto',
+  cursor: 'pointer',
+  fontSize: 22,
+  color: 'white',
+}
+
+// Footer Component
+// Class component
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className='footer-wrapper'>
+          <p>Copyright {this.props.date.getFullYear()}</p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    loggedIn: false,
+    techs: ['HTML', 'CSS', 'JS'],
+    message: 'Click show time or Greet people to change me',
+    background: {
+      background: "white",
+    },
+  }
+  handleLogin = () => {
+    this.setState({
+      loggedIn: !this.state.loggedIn,
+    })
+  }
+  handleSeason = () => {
+    let background = "";
+    let month = new Date().getMonth();
+    switch(true) {
+      case month < 3:
+        background = {background:"linear-gradient(0deg, #11998e 0%,#38ef7d 100% )"};
+        break;
+      case month >=3 && month < 6:
+        background = {background:"linear-gradient(0deg, #fc4a1a 0%,#f7b733 100% )"};
+        break;
+      case month >= 6 && month < 9:
+        background = {background:"linear-gradient(0deg, #f7971e 0%,#ffd200 100% )"};
+        break;
+      case month >= 9:
+        background = {background:"linear-gradient(0deg, #a8c0ff 0%,#3f2b96 100% )"};
+        break;
+      default:
+        background = {background:"linear-gradient(0deg, #11998e 0%,#38ef7d 100% )"};
+    }
+    this.setState({
+      background: background,
+    })
+  }
+  
+  showDate = (time) => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]
+
+    const month = months[time.getMonth()].slice(0, 3)
+    const year = time.getFullYear()
+    const date = time.getDate()
+    return `${month} ${date}, ${year}`
+  }
+  handleTime = () => {
+    let message = this.showDate(new Date())
+    this.setState({ message })
+  }
+  greetPeople = () => {
+    let message = 'Welcome to 30 Days Of React Challenge, 2020'
+    this.setState({ message })
+  }
+
+  render() {
+    const data = {
+      welcome: '30 Days Of React',
+      title: 'Getting Started React',
+      subtitle: 'JavaScript Library',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+      date: 'Oct 9, 2020',
+    }
+
+    return (
+      <div className='app'>
+        <Header data={data} />
+
+        <Main
+          techs={this.state.techs}
+          handleTime={this.handleTime}
+          greetPeople={this.greetPeople}
+          loggedIn={this.state.loggedIn}
+          handleLogin={this.handleSeason}
+          message={this.state.message}
+          background={this.state.background}
+        />
+
+        <Footer date={new Date()} />
+      </div>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
